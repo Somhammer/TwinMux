62cb9be28109808956b6944d752523d1f35730fc
Somhammer <klar.wind0425@gmail.com>
2020-05-06 17:34:31 +0900

    Debug RPConly efficiency

--- efficiency.py
+++ efficiency.py
@@ -8,18 +8,18 @@ from ROOT import *
 import tdrstyle
 import utils
 
-def calEfficiency():#(emulDir, inFile, outDir):
-    #print "Start "+str(inFile)
-    #if not os.path.exists(outDir+'/efficiency'):
-    #    os.makdirs(outDir+'/efficiency')
-    #f_out = TFile.Open(outDir+'/efficiency/'+inFile[:-5]+'_efficiency.root', 'recreate')
+def calculateEfficiency(args):#(emulDir, inFile, outDir):
+    inputDir = args[0]
+    inFile = args[1]
+    outDir = args[2]
+    multiProcess = args[3]
+    print "Start "+str(inFile)
+    if not os.path.exists(outDir+'/efficiency'):
+        os.makedirs(outDir+'/efficiency')
+    f_out = TFile.Open(outDir+'/efficiency/'+inFile, 'recreate')
     
-    #emulFile = inFile[:inFile.rfind('_')]+'_Emulator_'+inFile[:-5].split('_')[-1]+'.root'
-    #DTTREE_emul = TChain('dtNtupleProducer/DTTREE')
-    #DTTREE_emul.Add(emulDir+'/'+emulFile)
-    f_out = TFile.Open("Effciency.root","recreate")
-    DTTREE_emul = TChain('dtNtupleProducer/DTTREE')
-    DTTREE_emul.Add("DTDPGNtuple_10_3_3_ZMuSkim_2018D_emulator.root")
+    DTTREE = TChain('dtNtupleProducer/DTTREE')
+    DTTREE.Add(inputDir+'/'+inFile)
     f_out.cd()
     
     h_SegVsTwinMux_dPhi = TH1D('h_SegVsTwinMux_dPhi', 'Segment vs TwinMux dPhi', 100, -2000.0, 2000.0)
@@ -27,43 +27,70 @@ def calEfficiency():#(emulDir, inFile, outDir):
     h_SegVsTwinMux_dPhi.GetYaxis().SetTitle('Entries')
     h_SegVsTwinMux_dPhi.Sumw2()
 
+    h_Efficiency = TH1D('h_Efficiency', 'The efficiency of trigger primitives', 2, 0, 2)
+    h_Efficiency.GetXaxis().SetTitle("")
+    h_Efficiency.GetXaxis().SetBinLabel(1,'inclusive')
+    h_Efficiency.GetXaxis().SetBinLabel(2,'RPC only')
+    h_Efficiency.GetXaxis().SetTitle("Efficiency")
+    h_Efficiency.SetMaximum(1.2)
+    h_Efficiency.SetMinimum(0.0)
+
     coneSize = 1600
-    deno = 0 
-    nume = 0
-    for iEvt in xrange(DTTREE_emul.GetEntries()):
-        DTTREE_emul.GetEntry(iEvt)
-        for iSeg in range(DTTREE_emul.seg_nSegments):
-            if DTTREE_emul.seg_phi_t0[iSeg] > -6.0 or DTTREE_emul.seg_phi_t0[iSeg] < 6.0:
-                for iTrig in range(DTTREE_emul.ltTwinMuxOut_nTrigs):
-                    same_wheel = False
-                    same_station = False
-                    same_sector = False
+    denoInclusive = 0 
+    numeInclusive = 0
+    denoRPConly = 0
+    numeRPConly = 0
+    for ievent in xrange(DTTREE.GetEntries()):
+        if not multiProcess:
+            utils.printProgress(ievent, DTTREE.GetEntries(), 'Progress: ', 'Complete', 1, 25)
+        DTTREE.GetEntry(ievent)
+        for iseg in range(DTTREE.seg_nSegments):
+            if DTTREE.seg_phi_t0[iseg] > -6.0 or DTTREE.seg_phi_t0[iseg] < 6.0:
+                isMatchWithRPCbit01 = False
+                for itrig in range(DTTREE.ltTwinMuxOut_nTrigs):
+                    # Match between DTSegment and TwinMuxOut
+                    isSameDetector = False
+                    isSameWheel = False
+                    isSameStation = False
+                    isSameSector = False
                     isBX0 = False
 
-                    if DTTREE_emul.seg_wheel[iSeg] and DTTREE_emul.ltTwinMuxOut_wheel[iTrig]:
-                        same_wheel = True
-                    if DTTREE_emul.seg_sector[iSeg] and DTTREE_emul.ltTwinMuxOut_sector[iTrig]:
-                        same_sector = True
-                    if DTTREE_emul.seg_station[iSeg] and DTTREE_emul.ltTwinMuxOut_station[iTrig]:
-                        same_station = True
-                    if DTTREE_emul.ltTwinMuxOut_BX[iTrig] == 0:
+                    if DTTREE.seg_wheel[iseg] and DTTREE.ltTwinMuxOut_wheel[itrig]:
+                        isSameWheel = True
+                    if DTTREE.seg_sector[iseg] and DTTREE.ltTwinMuxOut_sector[itrig]:
+                        isSameSector = True
+                    if DTTREE.seg_station[iseg] and DTTREE.ltTwinMuxOut_station[itrig]:
+                        isSameStation = True
+                    if DTTREE.ltTwinMuxOut_BX[itrig] == 0:
                         isBX0 = True
 
-                    if same_wheel and same_station and same_sector and isBX0:
-                        deltaPhi = DTTREE_emul.seg_posGlb_phi[iSeg] - DTTREE_emul.ltTwinMuxOut_phi[iTrig]
+                    if isSameWheel and isSameStation and isSameSector and isBX0:
+                        deltaPhi = DTTREE.seg_posGlb_phi[iseg] - DTTREE.ltTwinMuxOut_phi[itrig]
                         h_SegVsTwinMux_dPhi.Fill(deltaPhi)
                     
-                    if DTTREE_emul.seg_station[iSeg] <= 2:
-                        deno += 1
-                        if DTTREE_emul.ltTwinMuxOut_rpcBit[iTrig] < 2 and abs(DTTREE_emul.ltTwinMuxOut_phi[iTrig]) < coneSize:
-                            nume += 1
+                    if DTTREE.seg_station[iseg] <= 2:
+                        denoInclusive += 1
+                        if DTTREE.ltTwinMuxOut_rpcBit[itrig] < 2:
+                            if abs(DTTREE.ltTwinMuxOut_phi[itrig]) < coneSize:
+                                numeInclusive += 1
+                                isMatchWithRPCbit01 = True
+                
+                if not isMatchWithRPCbit01:
+                    denoRPConly += 1
+                    for itrig in range(DTTREE.ltTwinMuxOut_nTrigs):
+                        if abs(DTTREE.ltTwinMuxOut_phi[itrig]) < coneSize and DTTREE.ltTwinMuxOut_rpcBit[itrig] == 2:
+                            numeRPConly += 1
 
-    print "Deno: "+str(deno)
-    print "Nume: "+str(nume)
-    eff = float(nume)/float(deno)
-    print "Efficiency: "+str(eff)
+    print "\nDeno(inclusive): "+str(denoInclusive)
+    print "Nume(inclusive): "+str(numeInclusive)
+    print "Deno(RPConly): "+str(denoRPConly)
+    print "Nume(RPConly): "+str(numeRPConly)
+    effInclusive = float(numeInclusive)/float(denoInclusive)
+    effRPConly = float(numeRPConly)/float(denoRPConly)
+    h_Efficiency.SetBinContent(1, effInclusive)
+    h_Efficiency.SetBinContent(2, effRPConly)
+    print "Efficiency(inclusive): "+str(effInclusive)
+    print "Efficiency(RPC only): "+str(effRPConly)
 
     f_out.Write()
     f_out.Close()
-
-calEfficiency()
