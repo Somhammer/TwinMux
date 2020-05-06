7f13a2b759dc391542d10055dd2dd382bcbe2dd0
Somhammer <klar.wind0425@gmail.com>
2020-05-06 17:36:53 +0900

    Chamber name is moved

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import sys
 from tqdm import tqdm
 
 def printProgress(iteration, total, prefix = '', suffix = '', decimals = 1, barLength = 100):
@@ -19,29 +20,30 @@ def str2bool(v):
     else:
         raise argparse.ArgumentTypeError('Boolean value expected')
 
+def argParser(*args):
+    arglist = [item for item in args]
+    print arglist
+    return arglist
+
 def setChamberName():
+    global nStation
+    global nWheel
+    global nSector
+    global nBX
     global name_RPCbit
     global name_Wheel
     global name_Sector
     global name_BX
 
-    global nStation
-    global nWheel
-    global nSector
-    global nBX
-    
+    nStation = 4 # 1 2 3 4
+    nWheel = 5 # -2 -1 0 1 2
+    nSector = 12 # 1 ~ 12
+    nBX = 9 # -4 ~  4 
     name_RPCbit = ['inclusive', 'RPCbit0', 'RPCbit1', 'RPCbit2']
     name_Wheel = ['W-2', 'W-1', 'W0', 'W+1', 'W+2']
     name_Sector = [
         'S1', 'S2', 'S3', 'S4', 'S5', 'S6',
-        'S7', 'S8', 'S9', 'S10', 'S11', 'S12']
-# We does not focus out and in in sector.
+        'S7', 'S8', 'S9', 'S10', 'S11', 'S12']# We does not focus out and in in sector.
     name_BX = ['BX-4', 'BX-3', 'BX-2', 'BX-1', 'BX0', 'BX1', 'BX2', 'BX3', 'BX4']
-
-    nStation = 4 # 1 2 3 4
-    nWheel = 5 # -2 -1 0 1 2
-    nSector = 12 # 1 ~ 12
-    nBX = 9 # -4 ~ 4
-
 
 
